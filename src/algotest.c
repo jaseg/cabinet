b21481ed2c26191cdbec5ea1b81b545a23df93d4
Jaseg <s@jaseg.de>
2011-07-18 16:08:14 +0200

    Now trying contour detection.

--- src/algotest.c
+++ src/algotest.c
@@ -47,27 +47,49 @@ int main(int argc, char** argv){
 	int from_to[] = {2,0}; //value channel extraction
 	mixChannels(&hsvimg, 1, &pimg, 1, from_to, 1);
 
-	Mat thrimg(img.size(), CV_8UC1);
-	adaptiveThreshold(pimg, thrimg, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 23, -23);
+	//Mat thrimg(img.size(), CV_8UC1);
+	//adaptiveThreshold(pimg, thrimg, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 23, -23);
 	
-	GaussianBlur(thrimg, pimg, Size(23, 23), 2.3, 2.3);
+	//GaussianBlur(thrimg, pimg, Size(23, 23), 2.3, 2.3);
 
-	Mat newthrimg(img.size(), CV_8UC1);
-	adaptiveThreshold(pimg, newthrimg, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 23, -42);
-
+	//Mat eqimg(img.size(), CV_8UC1);
+	//equalizeHist(pimg, eqimg);
+	Mat qimg(img.size(), CV_8UC1);
 	Mat displayimg(img.size(), CV_8UC3);
-	int chmux[] = {0,0, 0,1, 0,2};
-	mixChannels(&newthrimg, 1, &displayimg, 1, chmux, 3);
-	vector<Vec4i> lines;
-	HoughLinesP(newthrimg, lines, 1, CV_PI/180, 42, 42, 13);
-	for(vector<Vec4i>::iterator i = lines.begin(); i<lines.end(); i++){
-		line(displayimg, Point((*i)[0], (*i)[1]), Point((*i)[2], (*i)[3]), Scalar(0, 255, 0), 5);
-	}
+	//adaptiveThreshold(eqimg, newthrimg, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 23, -8);
+	threshold(pimg, pimg, 123, 255, THRESH_BINARY);
+	GaussianBlur(pimg, qimg, Size(23, 23), 5, 5);
+	threshold(qimg, qimg, 142, 255, THRESH_BINARY);
+	multiply(pimg, qimg, qimg);
+	//Canny(qimg, qimg, 42, 123);
+	//threshold(qimg, qimg, 127, 255, THRESH_BINARY);
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	findContours(qimg, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
 
+	int chmux[] = {0,0, 0,1, 0,2};
+	mixChannels(&qimg, 1, &displayimg, 1, chmux, 3);
+	vector<Vec4i> lines;
+	//HoughLinesP(qimg, lines, 1, CV_PI/180, 42, 42, 0);
+	int idx = 0;
+    for( ; idx >= 0; idx = hierarchy[idx][0] ){
+		Scalar color(rand()&255, rand()&255, rand()&255);
+		drawContours(displayimg, contours, idx, color, CV_FILLED, 8, hierarchy );
+	}
+	/*for(vector<Vec4i>::iterator i = lines.begin(); i<lines.end(); i++){
+		line(displayimg, Point((*i)[0], (*i)[1]), Point((*i)[2], (*i)[3]), Scalar(0, 255, 0), 5);
+	}*/
+
+	//namedWindow("pimg", 0);
+	//imshow("pimg", pimg);
+	//namedWindow("eqimg", 0);
+	//imshow("eqimg", eqimg);
 	namedWindow("displayimg", 0);
 	imshow("displayimg", displayimg);
-	namedWindow("newthrimg", 0);
-	imshow("newthrimg", newthrimg);
+	namedWindow("qimg", 0);
+	imshow("qimg", qimg);
+	//namedWindow("pimg", 0);
+	//imshow("pimg", pimg);
 	waitKey();
 	return 0;
 }
